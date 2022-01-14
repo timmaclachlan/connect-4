cd631f0b5f02ca9ddea4fde7c5cf2a76b12b8a9a
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-01-14 22:03:11 +0000

    Gamecircle with children id

---
+++ src/Components/GameCircle.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const onClick = () => {
+    alert('on click ');
+}
+
+const GameCircle = ({id, children}) => {
+    console.log(id);
+    return (
+        <div onClick={onClick}>
+            {children}
+        </div>
+    )
+}
+
+export default GameCircle;
