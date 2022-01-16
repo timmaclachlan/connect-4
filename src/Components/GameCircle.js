af777e451ea037f05ab76aadc123b98e1f21fa9b
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-01-16 16:29:41 +0000

    adding inline styles

--- src/Components/GameCircle.js
+++ src/Components/GameCircle.js
@@ -1,15 +1,25 @@
 import React from 'react'
 
-const onClick = () => {
-    alert('on click ');
+const onClick = (ev, id) => {
+
+    alert('on click ' + id);
 }
 
-const GameCircle = ({id, children}) => {
-    console.log(id);
+
+const GameCircle = ({id, color, children}) => {
+    const style = {
+        backgroundColor: color, 
+        width: 100, 
+        height: 100, 
+        margin: 10, 
+        borderRadius: '50%'
+    }
+
     return (
-        <div onClick={onClick}>
+        <div style={style} onClick={(ev) => onClick(ev, id)}>
             {children}
         </div>
+
     )
 }
 
